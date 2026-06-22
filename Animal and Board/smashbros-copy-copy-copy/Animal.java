3f58c5a6efce0f656da9969cc2705a7efc72ce08
BHASVIC-GabrielChan <gabriel.chan25@students.bhasvic.ac.uk>
2026-06-22 09:58:53 +0100

    INITSAKDFNSLKGFNBDS

---
+++ Animal and Board/smashbros-copy-copy-copy/Animal.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Animal here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Animal extends Spaces
+{
+    int levelOneStop;
+    int levelTwoStop;
+    int levelThreeStop;
+    int levelFourStop;
+    int cost;
+    Player ownedby;
+    public Animal (int Pspace, String Pname,int xCord,int yCord)
+    {
+        super(Pspace, Pname,xCord, yCord);                    
+    }
+    /**
+     * Act - do whatever the Animal wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
---
+++ Animal and Board/smashbros-copy-copy-copy/Spaces.java
@@ -0,0 +1,38 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Spaces here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Spaces extends Board
+{
+    protected int number = 100;
+    protected String name;
+    protected GreenfootImage background = null;
+    public Spaces(int Pnumber, String Pname,int xCord,int yCord)
+    {
+        this.number = Pnumber;
+        this.name = Pname;
+        background = new GreenfootImage (125,600);
+        background.setColor(Color.BLACK);
+        background.drawRect(xCord,yCord,125,100);
+        background.drawRect(xCord,yCord,125,25);
+        background.drawRect(xCord,yCord+75,125,25);
+        
+
+
+
+        
+        setImage(background);
+    }
+    /**
+     * Act - do whatever the Spaces wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
