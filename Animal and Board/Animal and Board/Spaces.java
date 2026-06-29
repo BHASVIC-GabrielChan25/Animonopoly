f605ea29933272c8a7252957c7198aa7f696fa15
BHASVIC-GabrielChan <gabriel.chan25@students.bhasvic.ac.uk>
2026-06-29 08:37:44 +0100

    zsd

--- Animal and Board/Animal and Board/Spaces.java
+++ Animal and Board/Animal and Board/Spaces.java
@@ -11,20 +11,17 @@ public class Spaces extends Actor
     protected int number = 100;
     protected String name;
     protected GreenfootImage background = null;
+    protected Font font= new Font(false, false, 11);
     public Spaces(int Pnumber, String Pname,int xCord,int yCord)
     {
         this.number = Pnumber;
         this.name = Pname;
         background = new GreenfootImage (126,101);
         background.setColor(Color.BLACK);
+        background.setFont(font);
         background.drawRect(xCord,yCord,125,100);
         background.drawRect(xCord,yCord,125,25);
         background.drawRect(xCord,yCord+75,125,25);
-        
-
-
-
-        
         setImage(background);
     }
     protected Player detectPlayer()
